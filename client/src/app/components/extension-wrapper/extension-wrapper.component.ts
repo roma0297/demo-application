be19c8c72f8d016537ae949288fe5d73dcd4de38
Capitan <oohla@mail.com>
2019-09-15 10:18:40 +0300

    Fix extension error

--- client/src/app/components/extension-wrapper/extension-wrapper.component.ts
+++ client/src/app/components/extension-wrapper/extension-wrapper.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { TObject } from 'src/common/types/market-place';
 
 @Component({
   selector: 'app-extension-wrapper',
@@ -7,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ExtensionWrapperComponent implements OnInit {
 
-  // @Input()
+  @Input() config: TObject;
 
   constructor() { }
 
